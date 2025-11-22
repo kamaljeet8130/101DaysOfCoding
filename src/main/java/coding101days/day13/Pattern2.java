2ae2c22c4441c8b51ac785f87bf16f4288a3b432
kamaljeet <kamaljeet8130@gmail.com>
2025-11-22 22:12:46 +0530

    Day13 Pattern2

--- src/main/java/coding101days/day13/Pattern2.java
+++ src/main/java/coding101days/day13/Pattern2.java
@@ -10,12 +10,13 @@ package coding101days.day13;
 */
 
 import java.util.Scanner;
+
 public class Pattern2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        for (int i = 1;i<=n;i++){
-            for(int j = 1;j<=n;j++){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
                 System.out.print(i + " ");
             }
             System.out.println();
