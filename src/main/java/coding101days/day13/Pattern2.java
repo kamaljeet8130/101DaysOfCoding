3a4dc121085642da051b3b33a281a0e1b16aff4a
kamaljeet <kamaljeet8130@gmail.com>
2025-11-22 22:11:33 +0530

    Day 13 Pattern1

---
+++ src/main/java/coding101days/day13/Pattern2.java
@@ -0,0 +1,24 @@
+package coding101days.day13;
+
+
+/*
+ Enter::
+3
+1 1 1
+2 2 2
+3 3 3
+*/
+
+import java.util.Scanner;
+public class Pattern2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 1;i<=n;i++){
+            for(int j = 1;j<=n;j++){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
