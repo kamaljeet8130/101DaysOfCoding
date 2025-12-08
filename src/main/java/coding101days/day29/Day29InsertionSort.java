591ba7ad08baffe17fd4c4a49491a2bc6be6c5d6
kamaljeet8130 <kamaljeet8130@gmail.com>
2025-12-08 18:57:47 +0530

    renaming of variables

--- src/main/java/coding101days/day29/Day29InsertionSort.java
+++ src/main/java/coding101days/day29/Day29InsertionSort.java
@@ -1,18 +1,26 @@
 package coding101days.day29;
 
 public class Day29InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {4,1,5,2,3};
+        insertionSort(arr);
+        printArray(arr);
+    }
 
     public static void insertionSort(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
-            int key = arr[i];
-            int j = i - 1;
-
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j--;
+            int current = arr[i];
+            int previous = i - 1;
+            while (previous >= 0 && arr[previous] > current) {
+                arr[previous+ 1] = arr[previous];
+                previous--;
             }
-
-            arr[j + 1] = key;
+            arr[previous + 1] = current;
+        }
+    }
+    public static void printArray(int[] arr){
+        for(int i = 0;i<arr.length;i++){
+            System.out.print(arr[i] + "");
         }
     }
 }
