6bf78b390bc0d9cb2efd53bedc5faedd830c6147
kamaljeet8130 <kamaljeet8130@gmail.com>
2025-12-16 18:22:22 +0530

    Day 38 Remove Duplicate from sorted Array

--- src/main/java/coding101days/day38/Day38RemoveDuplicatesFromSortedArray.java
+++ src/main/java/coding101days/day38/Day38RemoveDuplicatesFromSortedArray.java
@@ -2,25 +2,43 @@ package coding101days.day38;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.TreeSet;
 
 public class Day38RemoveDuplicatesFromSortedArray {
     public static void main(String[] args) {
         int[] arr = {0, 0, 3, 3, 5, 6};
-        for(int i = 1;i<arr.length-1;i++){
-            for (int j = 0;j<arr.length-1;j++){
-                if(arr[i] ==arr[j]){
-                    swap(arr,j,j+1);
-                }
-            }
+//        System.out.println(removeDuplicatesByUsingBruteForce(arr));
+        System.out.println(removeDuplicateByUsingOptimalSolution(arr));
 
-        }
-        for (int k: arr){
-            System.out.println(k);
-        }
     }
-    public static void swap(int[] arr,int num1,int num2){
-        int temp = arr[num1];
-        arr[num1] = arr[num2];
-        arr[num2] = temp;
+    public static int removeDuplicatesByUsingBruteForce(int[] arr){
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int i : arr){
+            set.add(i);
+        }
+        int count = 0;
+        for(int i : set){
+            arr[count++] = i;
+        }
+        for(int i : arr){
+            System.out.print(i + " ,");
+        }
+        System.out.println();
+        return count;
+    }
+    public static int removeDuplicateByUsingOptimalSolution(int[] arr){
+        //two pointer approach!!
+        int i = 0;
+        for(int j = 1;j<arr.length;j++){
+            if(arr[i]!=arr[j]){
+                arr[++i] = arr[j];
+            }
+        }
+        for (int j : arr){
+            System.out.print(j+" , ");
+        }
+        System.out.println();
+        return i;
     }
 }
+
