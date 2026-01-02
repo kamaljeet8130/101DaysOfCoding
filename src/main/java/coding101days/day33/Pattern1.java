1b08a41579af91744468d44a9198b0e2462250af
kamaljeet8130 <kamaljeet8130@gmail.com>
2026-01-02 18:45:05 +0530

    Day 31

---
+++ src/main/java/coding101days/day33/Pattern1.java
@@ -0,0 +1,17 @@
+package coding101days.day33;
+
+import java.util.Scanner;
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter :: ");
+        int n = scanner.nextInt();
+        for(int i =1;i<=n;i++){
+            for(int j = 1;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
